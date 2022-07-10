8405adb37b0cb829d5397fa786792c81e7bade1a
Mykola Bok <bmo.ossystem@gmail.com>
2022-07-10 18:31:32 +0300

    Update

--- src/modules/banners/banners.module.ts
+++ src/modules/banners/banners.module.ts
@@ -2,7 +2,7 @@ import { BannersService } from './banners.service';
 import { BannersController } from './banners.controller';
 import { Module } from '@nestjs/common';
 
-@Module({
+@Module({ 
   imports: [],
   controllers: [BannersController],
   providers: [BannersService],
